fc3dfcb14165077bef0642057f77f3f614ddde78
Dryvenn <dryvenn@gmail.com>
2016-08-30 17:49:01 +0200

    Package it all

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+# To use a consistent encoding
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='youtube_playlist_sync',
+    version='0.1.0',
+    description='Syncs YouTube playlists locally',
+    long_description=long_description,
+    url='https://github.com/dryvenn/youtube_playlist_sync',
+    author='dryvenn',
+    author_email='dryvenn@gmail.com',
+    license='MIT',
+    keywords='youtube playlist download dl sync',
+    packages=['youtube_playlist_sync'],
+    install_requires=[
+        'youtube_dl',
+    ],
+    entry_points={
+        'console_scripts': [
+            'youtube_playlist_sync=youtube_playlist_sync.__main__:main',
+        ],
+    },
+)
--- youtube_playlist_sync/__main__.py
+++ youtube_playlist_sync/__main__.py
@@ -2,20 +2,25 @@
 import sys
 import logging
 
-from sync import Playlist
-import config
+from . import config
+from .sync import Playlist
 
 
-logging.basicConfig(format='%(levelname)s: %(message)s', level=logging.DEBUG)
+def main():
+    logging.basicConfig(format='%(levelname)s: %(message)s', level=logging.DEBUG)
 
 
-exit_code = 0
+    exit_code = 0
 
-for conf in config.get().sections():
-    try:
-        Playlist(conf, config.get()[conf]['format'], config.get()[conf]['destination']).sync()
-    except RuntimeError as e:
-        logging.critical(str(e) + '\n')
-        exit_code = 1
+    for conf in config.get().sections():
+        try:
+            Playlist(conf, config.get()[conf]['format'], config.get()[conf]['destination']).sync()
+        except RuntimeError as e:
+            logging.critical(str(e) + '\n')
+            exit_code = 1
 
-exit(exit_code)
+    exit(exit_code)
+
+
+if __name__ == "__main__":
+    main()
